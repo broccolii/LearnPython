6d1d8881300eeb026c675dca4cd060263b7c834f
wujingpeng <wujingpeng@youzan.com>
2016-11-21 09:45:41 +0800

    Update some code

--- PreviouslyCode/First.py
+++ PreviouslyCode/First.py
@@ -1,18 +1,28 @@
-number = 23
-running = True
-while running:
-	guess = int(raw_input('Enter an integer : '))
-	if guess == number:
-		print 'Congratulations, you guessed it.' # New block starts here print "(but you do not win any prizes!)" # New block ends here 
-		break	
-	elif guess < number:
-		print 'No, it is a little higher than that' # Another block
-	# You can do whatever you want in a block ...
+#number = 23
+#running = True
+#while running:
+#	guess = int(raw_input('Enter an integer : '))
+#	if guess == number:
+#		print 'Congratulations, you guessed it.' # New block starts here print "(but you do not win any prizes!)" # New block ends here 
+#		break	
+#	elif guess < number:
+#		print 'No, it is a little higher than that' # Another block
+#	# You can do whatever you want in a block ...
+#	else:
+#		print 'No, it is a little lower than that'
+#	# you must have guess > number to reach here
+#		print 'Done'
+#	# This last statement is always executed, after the if statement is executed
+#else:
+#	print 'dddddd'
+#	
+
+def printMax(a, b):
+	if a > b:
+		print a,"is maximum"
 	else:
-		print 'No, it is a little lower than that'
-	# you must have guess > number to reach here
-		print 'Done'
-	# This last statement is always executed, after the if statement is executed
-else:
-	print 'dddddd'
-	
+		print b,'is maximum'
+
+printMax(4, 3)
+
+def say(message, times = 1): print message * times
