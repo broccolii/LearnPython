fd48062d065d5475f5197836942b87db976f1de8
wujingpeng <wujingpeng@youzan.com>
2016-11-28 11:34:40 +0800

    Update

--- Django/add_views/add_views/urls.py
+++ Django/add_views/add_views/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from calc import views as calc_views
 
 urlpatterns = [
+    url(r'^$', calc_views.index, name='home'),
+    url(r'^add/$', calc_views.add, name='add'),
+    url(r'^add/(\d+)/(\d+)/$', calc_views.add2, name='add2'),
     url(r'^admin/', admin.site.urls),
-    url(r'^add/$', calc_views.add, name="add")    
 ]
